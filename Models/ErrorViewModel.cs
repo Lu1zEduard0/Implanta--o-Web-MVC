95e959add05ecf152c214a242a8e0f52f609c846
Lu1z Eduard0 <luiz09edu@gmail.com>
2023-03-07 17:17:12 -0300

    Implantação Web MVC - SENAI

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Implantação_Web_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
